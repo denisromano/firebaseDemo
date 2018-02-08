d9ad207b967682f0152fb164b4c1588032e49b21
logonrm <logonrm@fiap.com.br>
2018-02-07 22:20:06 -0200

    Initial commit

---
+++ app/src/main/java/br/com/fiap/firebasedemo/MeuFirebaseInstanceIDService.java
@@ -0,0 +1,21 @@
+package br.com.fiap.firebasedemo;
+
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MeuFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+        // Obter o novo InstanceID
+        String firebaseToken = FirebaseInstanceId.getInstance().getToken();
+        //String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        String uid = FirebaseInstanceId.getInstance().getToken();
+
+        FirebaseDatabase.getInstance().getReference().child("utilizadores/"+uid+"/FirebaseToken")
+                .setValue(firebaseToken);
+
+    }
+}
